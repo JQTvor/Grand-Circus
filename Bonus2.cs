31937637aa7757c736f2774cbedb05cbb9e1b844
JQTvor <Jonnyhmi@yahoo.com>
2018-01-23 15:27:11 -0500

    Add files via upload

---
+++ Bonus2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus2
+{
+    class Bonus2
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Today;
+
+            Console.WriteLine("What is your birthdate?");
+            string sBDay = Console.ReadLine();
+            var BDay = Convert.ToDateTime(sBDay);
+
+            if(BDay.Day == today.Day && BDay.Month == today.Month)
+            {
+                Console.WriteLine("Happy Birthday!");
+            }
+            var time = today - BDay;
+            int days = time.Days;
+            int years = days / 365;
+
+          
+
+            Console.WriteLine("You are " + years + " years old!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
