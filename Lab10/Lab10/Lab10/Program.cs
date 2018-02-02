0dd153f9d6d560cfc20f45b3b5b5be4e86ac5e3c
Jonathan Higgins <Jonnyhmi@yahoo.com>
2018-02-02 16:21:26 -0500

    Add files via upload

---
+++ Lab10/Lab10/Lab10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TheMovies Movies = new TheMovies();
+
+            Console.WriteLine("Choose a catagory: \n H - Horror \n A - Animation \n S - SciFi \n D - Drama");
+            string input = Console.ReadLine().ToLower();
+
+
+
+            if(input == "h")
+            {
+                Movies.Horror();
+            }
+            if (input == "a")
+            {
+                Movies.Animation();
+            }
+            if (input == "d")
+            {
+                Movies.Drama();
+            }
+            if (input == "s")
+            {
+                Movies.SciFi();
+            }
+
+        }
+    }
+}
